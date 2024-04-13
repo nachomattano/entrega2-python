a52fad2a523ee487175a920972b2d6402660adf3
Nacho <ignacio.mattano199199@alumnos.info.unlp.edu.ar>
2024-04-12 22:25:57 -0300

    Inciso 3. Obtener mas influyente.

--- hockey/__init__.py
+++ hockey/__init__.py
@@ -2,15 +2,19 @@ def generate_stats(names, goals, goals_avoided, assists):
     stats = []
     names_list = names.split(", ")
     for i in range(len(names_list)):
-        jugador = {
+        player = {
             "Nombre": names_list[i],
             "Goles a favor": goals[i],
             "Goles evitados": goals_avoided[i],
             "Asistencias": assists[i]
         }
-        stats.append(jugador)
+        stats.append(player)
     return stats
 
 def max_scorer(stats):
     max_goals = max(stats, key=lambda x: x["Goles a favor"])
     return max_goals["Nombre"], max_goals["Goles a favor"]
+
+def most_influential(stats):
+    max_influential = max(stats, key=lambda x: x["Goles a favor"] * 1.5 + x["Goles evitados"] * 1.25 + x["Asistencias"])
+    return max_influential["Nombre"]
